e98b45851254fa9ca794ded16b30649ef4966ee6
Kanit Chanprasert <kanit_chan@hotmail.com>
2016-11-28 12:06:25 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,5 @@
 <?php
-$access_token = 'NA2ths2Q5rK5iqaje9r1jhEGtnXaMbym6CRgyG4bUzGjwYaZ47zsw62KnQ3VvgCS7TEa/YQ6MT1ksL4ZRFt8AnCUm9Sz/HBNlmHmjJ/Ajw6Hh9KElD7
-DR7SNv73zeqOkfYxZ+cN8Ag/F+ReUUVoJRgdB04t89/1O/w1cDnyilFU=';
+$access_token = 'NA2ths2Q5rK5iqaje9r1jhEGtnXaMbym6CRgyG4bUzGjwYaZ47zsw62KnQ3VvgCS7TEa/YQ6MT1ksL4ZRFt8AnCUm9Sz/HBNlmHmjJ/Ajw6Hh9KElD7DR7SNv73zeqOkfYxZ+cN8Ag/F+ReUUVoJRgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
